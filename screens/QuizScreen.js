edc75dea956886972b2f91828a6ecfb1427af271
jakecheong <jake.w.cheong@gmail.com>
2021-09-23 17:40:56 +0900

    quiz setting

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -19,17 +19,21 @@ export default class QuizScreen extends React.Component {
         quizNum: 0,
         answers: [],
         selectedAnswer: null,
-        result: []
+        result: [],
+        resultView: false,
+        startTime: null,
+        endTime: null
     }
 
 
     componentDidMount = async () => {
         await this.getQuizs()
         await this.setAnswers()
+        await this.setState({ loading: false, startTime: new Date() })
+        console.log(this.state.startTime)
     }
 
     getQuizs = async () => {
-        console.log('getQuizs')
         let jsonData = null
         try {
             await fetch('https://opentdb.com/api.php?amount=10&type=multiple')
@@ -37,8 +41,6 @@ export default class QuizScreen extends React.Component {
                 .then(json => jsonData = json)
 
             await this.setState({ quizs: jsonData.results })
-            await this.setState({ loading: false })
-
         } catch (e) {
             console.log(e)
         }
@@ -50,7 +52,7 @@ export default class QuizScreen extends React.Component {
             answers.push({ id: i, label: this.state.quizs[this.state.quizNum].incorrect_answers[i], value: this.state.quizs[this.state.quizNum].incorrect_answers[i] })
         }
         answers.push({ id: this.state.quizs[this.state.quizNum].incorrect_answers.length, label: this.state.quizs[this.state.quizNum].correct_answer, value: this.state.quizs[this.state.quizNum].correct_answer })
-        await this.setState({ answers: answers.sort(() => Math.random() - 0.5)})
+        await this.setState({ answers: answers.sort(() => Math.random() - 0.5)}) // 선택지 랜덤 출력
     }
 
     nextQuiz = async () => {
@@ -60,13 +62,27 @@ export default class QuizScreen extends React.Component {
 
     pressAnswer = async(answersArray) => {
         let selectedAnswer = null
+        let correct = null
         for(let i=0; i<answersArray.length; i++) {
             if(answersArray[i].selected) {
                 selectedAnswer = answersArray[i].value
             }
         }
-        await this.setState({ selectedAnswer: selectedAnswer })
+        if(this.state.quizs[this.state.quizNum].correct_answer == selectedAnswer) {
+            correct = true
+        } else {
+            correct = false
+        }
+        await this.setState({ selectedAnswer: selectedAnswer, correct: correct })
+
         this.setState({ showButton: true })
+    }
+
+    showResult = async () => { await this.setState({ resultView: true, endTime: new Date() })}
+
+    repeatQuiz = async () => { 
+        await this.setState({ quizNum: 0, resultView: false, showButton: false, startTime: new Date() })
+        await this.setAnswers()
     }
 
     render() {
@@ -102,22 +118,53 @@ export default class QuizScreen extends React.Component {
                     <View style={{ alignItems: 'center', margin: 30 }}>
                         <Quiz
                             question={this.state.quizs[this.state.quizNum].question}
-                            correct_answer={this.state.quizs[this.state.quizNum].correct_answer}
-                            incorrect_answers={this.state.quizs[this.state.quizNum].incorrect_answers}
+                            // correct_answer={this.state.quizs[this.state.quizNum].correct_answer}
+                            // incorrect_answers={this.state.quizs[this.state.quizNum].incorrect_answers}
                             answers={this.state.answers}
                             pressAnswer={this.pressAnswer}
                         />
                         {this.state.showButton ?
                             <View>
                                 {this.state.quizNum + 1 == this.state.quizs.length ?
-                                    <Button mode="contained" style={{ marginTop: 20 }}>
-                                        결과 보기
-                                    </Button>
+                                    <View style={styles.flexRow}>
+                                        {this.state.correct ?
+                                            <Text style={styles.rightText}>O  정답입니다!</Text>
+                                            :
+                                            <Text style={styles.wrongText}>X  오답입니다!</Text>
+                                        }
+                                        <Button mode="contained" onPress={this.showResult} disabled={this.state.resultView}>
+                                            결과 보기
+                                        </Button>
+                                    </View>
                                     :
-                                    <Button mode="contained" style={{ marginTop: 20 }} onPress={this.nextQuiz}>
-                                        다음 문항
-                                    </Button>
+                                    <View style={styles.flexRow}>
+                                        {this.state.correct ?
+                                            <Text style={styles.rightText}>O  정답입니다!</Text>
+                                            :
+                                            <Text style={styles.wrongText}>X  오답입니다!</Text>
+                                        }
+                                        <Button mode="contained" onPress={this.nextQuiz}>
+                                            다음 문항
+                                        </Button>
+                                    </View>
                                 }
+                            </View>
+                            :
+                            <View></View>
+                        }
+                        {this.state.resultView ?
+                            <View style={{ marginTop: 30 }}>
+                                <Text>총 시험시간: {(this.state.endTime.getTime() - this.state.startTime.getTime())/1000}초</Text>
+                                <Text>정답 수: </Text>
+                                <Text>오답 수: </Text>
+                                <View style={styles.flexRow}>
+                                    <Button mode="contained" onPress={this.repeatQuiz}>
+                                        다시 풀기
+                                    </Button>
+                                    <Button mode="contained" onPress={this.nextQuiz}>
+                                        오답 노트
+                                    </Button>
+                                </View>
                             </View>
                             :
                             <View></View>
@@ -134,5 +181,22 @@ const styles = StyleSheet.create({
         fontSize: 15,
         fontWeight: 'bold',
         margin: 5
+    },
+    flexRow: {
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'space-between', 
+        width: '70%', 
+        marginTop: 20
+    },
+    rightText: {
+        fontSize: 16, 
+        color:Colors.green900,
+        fontWeight:'bold'
+    },
+    wrongText: {
+        fontSize: 16, 
+        color:Colors.red600,
+        fontWeight:'bold'
     }
 })
